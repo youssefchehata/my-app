5605153e480260168067c3336ac02c5f71d50dad
youssef <jsf.ch6@gmail.com>
2022-07-29 22:26:41 +0100

    first-commit

---
+++ src/store/store.tsx
@@ -0,0 +1,20 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers/rootReducer';
+
+const reducer = rootReducer;
+
+
+
+const initialState = {
+
+};
+const middleware = [thunk];
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
